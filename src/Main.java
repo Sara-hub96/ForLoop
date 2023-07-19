de8ac0f5c06c31f4ca35f68df4845ad17ca2dc54
Sara <saracoppola996@gmail.com>
2023-07-19 23:26:23 +0200

    printed numbers

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/* For Loop
+    Use a for-loop to print all the number from 5 to 25 (including the upper and lower bound);*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 5; i <= 25; i++){
+            System.out.println(i);
+        }
+    }
+}
